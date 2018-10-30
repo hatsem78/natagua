32c355f19af3c2cbd97394faa5fba5767812ae49
octavio <hatsem78@gmail.com>
2018-10-30 06:47:21 -0300

    Preliminar

--- natagua/settings.py
+++ natagua/settings.py
@@ -130,4 +130,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+# Static files (CSS, JavaScript, Images)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [  # For static files not particular to any app.
+    os.path.join(PROJECT_DIR, ''),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL =  STATIC_ROOT +'/static/'  # eg 127.0.0.1/static/
